cbf694ecaae86ff31b3958ea51d6aee9a3170ce6
duola <huyinbing@gmail.com>
2014-05-31 18:26:09 +0800

    download url

--- TiebaMessageReviewButton/BaiduTiebaMessageReviewButton.user.js
+++ TiebaMessageReviewButton/BaiduTiebaMessageReviewButton.user.js
@@ -2,8 +2,8 @@
 // @name			Baidu Tieba Message Review Button
 // @description		把贴吧T豆帐单更改为查看贴吧消息按纽
 // @author			哆啦B梦的弟弟
-// @updateURL		https://github.com/duola/gm_script/raw/master/TiebaMessageReviewButton/baidutiebamessagereviewbutton.user.js
-// @downloadURL		https://github.com/duola/gm_script/raw/master/TiebaMessageReviewButton/baidutiebamessagereviewbutton.user.js
+// @updateURL		https://raw.githubusercontent.com/duola/gm_script/master/TiebaMessageReviewButton/BaiduTiebaMessageReviewButton.user.js
+// @downloadURL		https://raw.githubusercontent.com/duola/gm_script/master/TiebaMessageReviewButton/BaiduTiebaMessageReviewButton.user.js
 // @include			http://tieba.baidu.com/*
 // @require			http://code.jquery.com/jquery-1.6.min.js
 // @version			2014.05.31
